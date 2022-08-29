6b6922aa06a656ec02734ae469e166ee1301d61b
Bruno Monteiro <104601906+Brunomon2812@users.noreply.github.com>
2022-08-29 08:03:33 -0400

    to-do-list updated

--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -1,35 +1,16 @@
-import express from "express"
-import knex from "knex"
-import dotenv from "dotenv"
+import express, { Express } from "express";
+import { AddressInfo } from "net";
+import endpoints from './routes/endpoints'
 
-dotenv.config()
-
-export const connection = knex({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: 3306
-
-    }
-})
-
-const app = express();
-app.use(express.json())
+const app: Express = express();
+app.use("/", endpoints)
 
 
-
-
- app.listen(process.env.PORT || 3003, () => {
-   
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
       console.log(`Server is running in http://localhost:${address.port}`);
-  }
-);
-
-
-
-
-
-
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+});
