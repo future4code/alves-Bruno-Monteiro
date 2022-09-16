33868d6f2c6f1379aaa19558a8b0dd05034c5fbc
Bruno Monteiro <104601906+Brunomon2812@users.noreply.github.com>
2022-09-16 19:35:00 -0400

    Cookenu Project Completed

--- modulo5/Cookenu/src/endpoints/users/signup.ts
+++ modulo5/Cookenu/src/endpoints/users/signup.ts
@@ -12,7 +12,24 @@ export default async function signup(
     try {
         
         const { name, email, password, } = req.body
+
+        if (!name || !email || !password) {
+            res.statusCode = 422
+            throw new Error ("'name', 'email' and 'password' required")
+        }
+
+        if (password.length <6) {
+            res.statusCode = 422
+            throw new Error ("'password', must be at least 6 characters long")
+        }
         
+        const [user] = await connection(userTableName)
+        .where({email})
+
+        if (user){
+            res.statusCode = 409
+            throw new Error("Email already in use")
+        }
         const id: string = generateId()
         
         const cypherPassword: string = generateHash(password)
